dd0f0c624709141e4c976ad5ad245159fde6fb16
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2021-03-27 14:36:51 +0200

    Update LoginPage.js

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,97 +1,91 @@
-import React, { Component } from "react";
+import React, { useState, useCallback } from "react";
+import { useDispatch } from "react-redux";
 import { loginUser } from "../../redux/contacts/contactsOperations";
-import { statusRemember } from "../../redux/status/statusActions";
-import { connect } from "react-redux";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const initialState = {
-  email: "",
-  password: "",
-};
+export default function LoginPage() {
+  const dispatch = useDispatch();
 
-class LoginPage extends Component {
-  state = {
+  const [inputValues, setInputValues] = useState({
     email: "",
     password: "",
-  };
+  });
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
+  const handleChange = useCallback(
+    ({ target }) => {
+      setInputValues({ ...inputValues, [target.name]: target.value });
+    },
+    [inputValues]
+  );
 
-  handleSubmit = () => {
-    this.props.loginUser(this.state);
-    this.setState({ ...initialState });
-  };
+  // const [checked, setChecked] = useState();
 
-  handleCheked = (e) => {
-    const { statusRemember } = this.props;
-    const check = e.target.checked;
-    statusRemember(check);
-  };
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
 
-  render() {
-    const { email, password } = this.state;
-    return (
-      <Container>
-        <br />
-        <Row className="justify-content-md-center">
-          <Col xs lg="5">
-            <h2>Вход</h2>
-            <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Введите email"
-                  suggested="email"
-                  name="email"
-                  value={email}
-                  autoComplete="email"
-                  onChange={this.handleChange}
-                />
-                <Form.Text className="text-muted">
-                  Мы никому не передаем адрес вашей электронной почты.
-                </Form.Text>
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Пароль</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Введите пароль"
-                  suggested="password"
-                  name="password"
-                  value={password}
-                  autoComplete="password"
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check
-                  type="checkbox"
-                  label="Запомнить меня"
-                  onChange={this.handleCheked}
-                />
-              </Form.Group>
-              <Button
-                variant="primary"
-                type="button"
-                onClick={this.handleSubmit}
-              >
-                Войти
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+      dispatch(loginUser(inputValues));
+
+      setInputValues({ name: "", password: "" });
+    },
+    [dispatch, inputValues]
+  );
+
+  // handleCheked = (e) => {
+  //   const { statusRemember } = this.props;
+  //   const check = e.target.checked;
+  //   statusRemember(check);
+  // };
+
+  return (
+    <Container>
+      <br />
+      <Row className="justify-content-md-center">
+        <Col xs lg="5">
+          <h2>Вход</h2>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Введите email"
+                suggested="email"
+                name="email"
+                value={inputValues.email}
+                autoComplete="email"
+                onChange={handleChange}
+              />
+              <Form.Text className="text-muted">
+                Мы никому не передаем адрес вашей электронной почты.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Пароль</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Введите пароль"
+                suggested="password"
+                name="password"
+                value={inputValues.password}
+                autoComplete="password"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {/* <Form.Group controlId="formBasicCheckbox">
+              <Form.Check
+                type="checkbox"
+                label="Запомнить меня"
+                checked={checked}
+                onChange={setChecked}
+              />
+            </Form.Group> */}
+            <Button variant="primary" type="button" onClick={handleSubmit}>
+              Войти
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
-
-const mapDispatchToProps = {
-  loginUser,
-  statusRemember,
-};
-
-export default connect(null, mapDispatchToProps)(LoginPage);

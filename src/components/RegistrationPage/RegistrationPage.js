7ae955ba647305ed6b6a68886680d37027af5320
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2021-03-27 15:51:51 +0200

    Update RegistrationPage.js

--- src/components/RegistrationPage/RegistrationPage.js
+++ src/components/RegistrationPage/RegistrationPage.js
@@ -1,145 +1,110 @@
-import React, { Component } from "react";
+import React, { useState, useCallback } from "react";
+import { useDispatch } from "react-redux";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { connect } from "react-redux";
 import { registrationUser } from "../../redux/contacts/contactsOperations";
 import { registrationError } from "../../redux/contacts/contactsActions";
 
-const initialState = {
-  firstName: "",
-  secondName: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
-};
+export default function RegistrationPage() {
+  const dispatch = useDispatch();
 
-class RegistrationPage extends Component {
-  state = {
-    firstName: "",
-    secondName: "",
+  const [inputValues, setInputValues] = useState({
+    name: "",
     email: "",
     password: "",
     confirmPassword: "",
-  };
+  });
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
+  const handleChange = useCallback(
+    ({ target }) => {
+      setInputValues({ ...inputValues, [target.name]: target.value });
+    },
+    [inputValues]
+  );
 
-  handleSubmit = () => {
-    const {
-      firstName,
-      secondName,
-      email,
-      password,
-      confirmPassword,
-    } = this.state;
-    const user = {
-      name: `${firstName} ${secondName}`,
-      email: email,
-      password: password,
-    };
-    if (password !== confirmPassword) {
-      this.props.registrationError("password dismach");
-      return;
-    } else {
-      this.props.registrationUser(user);
-    }
-    this.setState({ ...initialState });
-  };
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (inputValues.password === inputValues.confirmPassword) {
+        dispatch(registrationUser(inputValues));
+      } else {
+        dispatch(registrationError("password dismach"));
+      }
 
-  render() {
-    const {
-      firstName,
-      secondName,
-      confirmPassword,
-      email,
-      password,
-    } = this.state;
-    return (
-      <Container>
-        <br />
-        <Row className="justify-content-md-center">
-          <Col xs lg="5">
-            <h2>Регистрация</h2>
-            <Form type="form">
-              <Form.Group controlId="firstName">
-                <Form.Label>Имя</Form.Label>
-                <Form.Control
-                  type="firstName"
-                  name="firstName"
-                  value={firstName}
-                  placeholder="Имя"
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="secondName">
-                <Form.Label>Фамилия</Form.Label>
-                <Form.Control
-                  type="secondName"
-                  name="secondName"
-                  value={secondName}
-                  placeholder="Фамилия"
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  suggested="email"
-                  autoComplete="email"
-                  type="email"
-                  name="email"
-                  value={email}
-                  placeholder="Введите email"
-                  onChange={this.handleChange}
-                />
-                <Form.Text className="text-muted">
-                  Мы никому не передаем адрес вашей электронной почты.
-                </Form.Text>
-              </Form.Group>
-              <Form.Group controlId="password">
-                <Form.Label>Пароль</Form.Label>
-                <Form.Control
-                  suggested="password"
-                  autoComplete="new-password"
-                  type="password"
-                  name="password"
-                  value={password}
-                  placeholder="Ведите пароль"
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="confirmPassword">
-                <Form.Label>Подтверждение пароля</Form.Label>
-                <Form.Control
-                  suggested="confirmPassword"
-                  autoComplete="confirmPassword"
-                  type="password"
-                  name="confirmPassword"
-                  value={confirmPassword}
-                  placeholder="Подтвердите пароль"
-                  onChange={this.handleChange}
-                />
-              </Form.Group>
-              <Button
-                variant="primary"
-                type="button"
-                onClick={this.handleSubmit}
-              >
-                Регистрация
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+      setInputValues({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+    },
+
+    [dispatch, inputValues]
+  );
+
+  return (
+    <Container>
+      <br />
+      <Row className="justify-content-md-center">
+        <Col xs lg="5">
+          <h2>Регистрация</h2>
+          <Form type="form">
+            <Form.Group controlId="firstName">
+              <Form.Label>Имя</Form.Label>
+              <Form.Control
+                type="name"
+                name="name"
+                value={inputValues.name}
+                placeholder="Имя"
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                suggested="email"
+                autoComplete="email"
+                type="email"
+                name="email"
+                value={inputValues.email}
+                placeholder="Введите email"
+                onChange={handleChange}
+              />
+              <Form.Text className="text-muted">
+                Мы никому не передаем адрес вашей электронной почты.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Пароль</Form.Label>
+              <Form.Control
+                suggested="password"
+                autoComplete="new-password"
+                type="password"
+                name="password"
+                value={inputValues.password}
+                placeholder="Ведите пароль"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="confirmPassword">
+              <Form.Label>Подтверждение пароля</Form.Label>
+              <Form.Control
+                suggested="confirmPassword"
+                autoComplete="confirmPassword"
+                type="password"
+                name="confirmPassword"
+                value={inputValues.confirmPassword}
+                placeholder="Подтвердите пароль"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button variant="primary" type="button" onClick={handleSubmit}>
+              Регистрация
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
-
-const mapDispatchToProps = {
-  registrationUser,
-  registrationError,
-};
-
-export default connect(null, mapDispatchToProps)(RegistrationPage);
